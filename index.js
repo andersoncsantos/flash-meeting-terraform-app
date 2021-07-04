cf9a3ae1eed28f5bff3cd51dcbfd15e860451142
Anderson Santos <anderson.santos@iteris.com.br>
2021-07-04 19:25:24 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+﻿const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("Flash Meeting Terraform!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
